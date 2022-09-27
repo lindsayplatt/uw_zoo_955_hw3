2efc54639763d702d519965dbb67c2ad67f0e960
Daniel Szydlowski <dszydlowski@wisc.edu>
2022-09-27 14:00:35 -0500

    figured out which fish change sex

--- Homework3.R
+++ Homework3.R
@@ -1,11 +1,22 @@
 library(xlsx)
 library(tidyverse)
+library(lubridate)
 
+
+
+# reading in the data
 data = read.xlsx("BSB_tagging_data.xlsx", sheetIndex = 1)
 
+# removing NA values from the data
 data <- data %>% filter(!is.na(Date_at_recapture)) %>% 
   filter(!is.na(Date_at_capture)) %>% 
   filter(!is.na(Length_at_capture))
+
+data <- data %>% mutate(month = month(Date_at_recapture))
+
+lateSeason <- data %>% filter(month > 7)
+
+lateSeason <- lateSeason %>% mutate(changed = (Sex_at_capture == "F" & Sex_at_recapture != "F"))
 
 
 
